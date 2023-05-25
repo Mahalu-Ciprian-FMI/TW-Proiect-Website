45b1d355426cec29fc9b8e84f0a0755cad0e1153
Mahalu-Ciprian-FMI <128313829+Mahalu-Ciprian-FMI@users.noreply.github.com>
2023-05-25 11:33:17 +0300

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,7 +1,12 @@
+var http = require('http');
+var express=require('express');
+var app=express();
+
 app.use(function(req,res,next){
-    res.status(404).render()
+    res.status(404).render("404");
 })
-app.listen(3000,()=>
-{
-    console.log("test")
-});
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(3004);
